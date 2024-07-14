ff44c382c4b13d268545b14b404d76926969e399
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2024-07-14 17:44:34 +0300

    session5 and task

---
+++ session5/session5_task/assets/include/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    
+
+<ul style="display: flex; gap: 50px; list-style: none; margin-bottom: 5px">
+    <li>
+        <a href="index.php" style="text-decoration: none">Home</a>
+    </li>
+    <li>
+        <a href="contact.php" style="text-decoration: none">contact</a>
+    </li>
+    <li>
+        <a href="products.php" style="text-decoration: none">products</a>
+    </li>
+</ul>
+
